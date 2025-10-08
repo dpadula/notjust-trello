831396c0ce745a2e3eebe28c676df8a334940f24
Diego <dpadula@epe.santafe.gov.ar>
2025-10-08 10:45:08 -0300

    Cambiamos por stack para que no haga flick cuando cambiamos de ruta. Es el equivalente a SPA en web

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,11 @@
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 
 export default function RootLayout() {
   return (
     <>
-      <Slot />
+      <Stack />
       <StatusBar style='light' />
     </>
   );
